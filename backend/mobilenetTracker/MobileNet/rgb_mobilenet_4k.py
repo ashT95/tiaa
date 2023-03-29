950f16e721953938deb36e2f4bd5d8d74c8575b2
ashT95 <ash@moeyinc.com>
2023-03-29 17:12:27 -0400

    letterboxing update

--- backend/mobilenetTracker/MobileNet/rgb_mobilenet_4k.py
+++ backend/mobilenetTracker/MobileNet/rgb_mobilenet_4k.py
@@ -7,6 +7,13 @@ import depthai as dai
 import numpy as np
 import time
 
+'''
+Mobilenet SSD device side decoding demo
+  The "mobilenet-ssd" model is a Single-Shot multibox Detection (SSD) network intended
+  to perform object detection. This model is implemented using the Caffe* framework.
+  For details about this model, check out the repository <https://github.com/chuanqi305/MobileNet-SSD>.
+'''
+
 # Get argument first
 nnPath = str((Path(__file__).parent / Path('../models/mobilenet-ssd_openvino_2021.4_6shave.blob')).resolve().absolute())
 if len(sys.argv) > 1:
@@ -26,48 +33,37 @@ syncNN = True
 pipeline = dai.Pipeline()
 
 # Define sources and outputs
-camRgb = pipeline.create(dai.node.ColorCamera)
-spatialDetectionNetwork = pipeline.create(dai.node.MobileNetSpatialDetectionNetwork)
 monoLeft = pipeline.create(dai.node.MonoCamera)
 monoRight = pipeline.create(dai.node.MonoCamera)
 stereo = pipeline.create(dai.node.StereoDepth)
+spatialDetectionNetwork = pipeline.create(dai.node.MobileNetSpatialDetectionNetwork)
+imageManip = pipeline.create(dai.node.ImageManip)
 
-xoutRgb = pipeline.create(dai.node.XLinkOut)
-xoutPreview = pipeline.create(dai.node.XLinkOut)
-xoutNN = pipeline.create(dai.node.XLinkOut)
+xoutManip = pipeline.create(dai.node.XLinkOut)
+nnOut = pipeline.create(dai.node.XLinkOut)
 xoutDepth = pipeline.create(dai.node.XLinkOut)
 
-xoutRgb.setStreamName("rgb")
-xoutPreview.setStreamName("preview")
-xoutNN.setStreamName("detections")
+xoutManip.setStreamName("right")
+nnOut.setStreamName("detections")
 xoutDepth.setStreamName("depth")
 
-dai.Device().setIrFloodLightBrightness(100)
-dai.Device().setIrLaserDotProjectorBrightness(100)
-
-# self.device.setIrLaserDotProjectorBrightness(50)
-# self.device.setIrFloodLightBrightness(50)
-
 # Properties
-camRgb.setPreviewSize(300, 300)    # NN input
-camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
-camRgb.setInterleaved(False)
-camRgb.setPreviewKeepAspectRatio(False)
-camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+imageManip.initialConfig.setResizeThumbnail(300, 300)
+# The NN model expects BGR input. By default ImageManip output type would be same as input (gray in this case)
+imageManip.initialConfig.setFrameType(dai.ImgFrame.Type.BGR888p)
+
 
 monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
 monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
 monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
 monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 
-# Setting node configs
+# StereoDepth
 stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
-# Align depth map to the perspective of RGB camera, on which inference is done
-stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
-stereo.setOutputSize(monoLeft.getResolutionWidth(), monoLeft.getResolutionHeight())
 
-spatialDetectionNetwork.setBlobPath(nnPath)
+# Define a neural network that will make predictions based on the source frames
 spatialDetectionNetwork.setConfidenceThreshold(0.5)
+spatialDetectionNetwork.setBlobPath(nnPath)
 spatialDetectionNetwork.input.setBlocking(False)
 spatialDetectionNetwork.setBoundingBoxScaleFactor(0.5)
 spatialDetectionNetwork.setDepthLowerThreshold(100)
@@ -76,145 +72,94 @@ spatialDetectionNetwork.setDepthUpperThreshold(5000)
 # Linking
 monoLeft.out.link(stereo.left)
 monoRight.out.link(stereo.right)
-camRgb.preview.link(spatialDetectionNetwork.input)
 
+imageManip.out.link(spatialDetectionNetwork.input)
 if syncNN:
-    spatialDetectionNetwork.passthrough.link(xoutRgb.input)
+    spatialDetectionNetwork.passthrough.link(xoutManip.input)
 else:
-    camRgb.preview.link(xoutRgb.input)
+    imageManip.out.link(xoutManip.input)
 
-spatialDetectionNetwork.out.link(xoutNN.input)
+spatialDetectionNetwork.out.link(nnOut.input)
 
+stereo.rectifiedRight.link(imageManip.inputImage)
 stereo.depth.link(spatialDetectionNetwork.inputDepth)
 spatialDetectionNetwork.passthroughDepth.link(xoutDepth.input)
 
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
 
-    # Output queues will be used to get the frames and nn data from the outputs defined above
-    qVideo = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
-    qPreview = device.getOutputQueue(name="preview", maxSize=4, blocking=False)
-    qDet = device.getOutputQueue(name="detections", maxSize=4, blocking=False)
+    # Output queues will be used to get the rgb frames and nn data from the outputs defined above
+    previewQueue = device.getOutputQueue(name="right", maxSize=4, blocking=False)
+    detectionNNQueue = device.getOutputQueue(name="detections", maxSize=4, blocking=False)
     depthQueue = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
+
+    rectifiedRight = None
+    detections = []
 
     startTime = time.monotonic()
     counter = 0
     fps = 0
     color = (255, 255, 255)
 
-    previewFrame = None
-    videoFrame = None
-    detections = []
+    while True:
+        inRectified = previewQueue.get()
+        inDet = detectionNNQueue.get()
+        inDepth = depthQueue.get()
 
-    # nn data, being the bounding box locations, are in <0..1> range - they need to be normalized with frame width/height
-    def frameNorm(frame, bbox):
-        normVals = np.full(len(bbox), frame.shape[0])
-        normVals[::2] = frame.shape[1]
-        return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
+        counter += 1
+        currentTime = time.monotonic()
+        if (currentTime - startTime) > 1:
+            fps = counter / (currentTime - startTime)
+            counter = 0
+            startTime = currentTime
 
-    def displaySpatializedFrame(name, frame):
-        # If the frame is available, draw bounding boxes on it and show the frame
-        height = frame.shape[0]
-        width  = frame.shape[1]
-        color = (255, 0, 0)
+        rectifiedRight = inRectified.getCvFrame()
 
+        depthFrame = inDepth.getFrame() # depthFrame values are in millimeters
+
+        depthFrameColor = cv2.normalize(depthFrame, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
+        depthFrameColor = cv2.equalizeHist(depthFrameColor)
+        depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
+
+        detections = inDet.detections
+
+        # If the rectifiedRight is available, draw bounding boxes on it and show the rectifiedRight
+        height = rectifiedRight.shape[1]
+        width = rectifiedRight.shape[1]
         for detection in detections:
+            roiData = detection.boundingBoxMapping
+            roi = roiData.roi
+            roi = roi.denormalize(depthFrameColor.shape[1], depthFrameColor.shape[0])
+            topLeft = roi.topLeft()
+            bottomRight = roi.bottomRight()
+            xmin = int(topLeft.x)
+            ymin = int(topLeft.y)
+            xmax = int(bottomRight.x)
+            ymax = int(bottomRight.y)
+            cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), color, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX)
+
+            # Denormalize bounding box
+            x1 = int(detection.xmin * width)
+            x2 = int(detection.xmax * width)
+            y1 = int(detection.ymin * height)
+            y2 = int(detection.ymax * height)
+
             try:
                 label = labelMap[detection.label]
             except:
                 label = detection.label
 
-            # Only detect people
-            if label == "person":
-                bbox = frameNorm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
-                cv2.putText(frame, labelMap[detection.label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
-                cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
-                cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 2)
+            cv2.putText(rectifiedRight, str(label), (x1 + 10, y1 + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(rectifiedRight, "{:.2f}".format(detection.confidence*100), (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(rectifiedRight, f"X: {int(detection.spatialCoordinates.x)} mm", (x1 + 10, y1 + 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(rectifiedRight, f"Y: {int(detection.spatialCoordinates.y)} mm", (x1 + 10, y1 + 65), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(rectifiedRight, f"Z: {int(detection.spatialCoordinates.z)} mm", (x1 + 10, y1 + 80), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
 
-                roiData = detection.boundingBoxMapping
-                roi = roiData.roi
-                roi = roi.denormalize(depthFrameColor.shape[1], depthFrameColor.shape[0])
-                topLeft = roi.topLeft()
-                bottomRight = roi.bottomRight()
-                xmin = int(topLeft.x)
-                ymin = int(topLeft.y)
-                xmax = int(bottomRight.x)
-                ymax = int(bottomRight.y)
-                cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), color, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX)
+            cv2.rectangle(rectifiedRight, (x1, y1), (x2, y2), color, cv2.FONT_HERSHEY_SIMPLEX)
 
-                # Denormalize bounding box
-                x1 = int(detection.xmin * width)
-                x2 = int(detection.xmax * width)
-                y1 = int(detection.ymin * height)
-                y2 = int(detection.ymax * height)
-            
-                cv2.putText(frame, str(label), (x1 + 10, y1 + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-                cv2.putText(frame, f"X: {int(detection.spatialCoordinates.x)} mm", (x1 + 10, y1 + 65), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-                cv2.putText(frame, f"Y: {int(detection.spatialCoordinates.y)} mm", (x1 + 10, y1 + 80), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-                cv2.putText(frame, f"Z: {int(detection.spatialCoordinates.z)} mm", (x1 + 10, y1 + 95), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+        cv2.putText(rectifiedRight, "NN fps: {:.2f}".format(fps), (2, rectifiedRight.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color)
+        cv2.imshow("depth", depthFrameColor)
+        cv2.imshow("rectified right", rectifiedRight)
 
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), cv2.FONT_HERSHEY_SIMPLEX)
-
-                # cX = x1+(x2-x1)/2
-                # CY = y1
-                
-                # frame = cv2.circle(frame, (cX, cY), 10, (255, 0, 0), 3)
-                
-                # --------------------------------------SENDING COORDINATES TO ELECTRON MAIN---------------------------------------------------- #
-                print(f"X:{int(detection.spatialCoordinates.x)},Y:{int(detection.spatialCoordinates.y)},Z:{int(detection.spatialCoordinates.z)}")
-                sys.stdout.flush()
-                # ------------------------------------------------------------------------------------------------------------------------------- #
-
-        cv2.putText(frame, "NN fps: {:.2f}".format(fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, (255,255,255))
-        #cv2.imshow("depth", depthFrameColor)
-        cv2.imshow(name, frame)
-
-     cv2.namedWindow("rgb", cv2.WINDOW_NORMAL)
-     cv2.resizeWindow("rgb", 1280, 720)
-
-
-    def displayFrame(name, frame):
-        color = (255, 0, 0)
-        for detection in detections:
-            bbox = frameNorm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
-            cv2.putText(frame, labelMap[detection.label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
-            cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
-            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 2)
-        # Show the frame
-    cv2.imshow(name, frame)
-
-    while True:
-        # Instead of get (blocking), we use tryGet (non-blocking) which will return the available data or None otherwise
-        inVideo = qVideo.tryGet()
-        inPreview = qPreview.tryGet()
-        inDet = qDet.tryGet()
-        depth = depthQueue.get()
-        
-        counter+=1
-        current_time = time.monotonic()
-        if (current_time - startTime) > 1 :
-            fps = counter / (current_time - startTime)
-            counter = 0
-            startTime = current_time
-
-        if depth is not None:
-            depthFrame = depth.getFrame() # depthFrame values are in millimeters
-            depthFrameColor = cv2.normalize(depthFrame, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
-            depthFrameColor = cv2.equalizeHist(depthFrameColor)
-            depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
-
-        if inVideo is not None:
-            videoFrame = inVideo.getCvFrame()
-
-        if inPreview is not None:
-            previewFrame = inPreview.getCvFrame()
-
-        if inDet is not None:
-            detections = inDet.detections
-
-        if videoFrame is not None:
-            displaySpatializedFrame("rgb", videoFrame)
-            # displayFrame("rgb", videoFrame)
-        
         if cv2.waitKey(1) == ord('q'):
             break
